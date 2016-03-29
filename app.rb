e108ec6e35f1aece29de2f98751e3362ef566ebf
Nadia Barbosa <nadiabarbosa@me.com>
2016-03-29 15:28:24 -0400

    Create list employees endpoint and get individual employee endpoint

--- app.rb
+++ app.rb
@@ -3,10 +3,30 @@ require 'bundler/setup'
 require 'sinatra' # Will start web server
 require 'active_record'
 require 'pry'
+# Initial seed of DB
+require './development'
 
-# Set up production DB
+#Set up production DB
 # require './production_db'
 
+before do
+  content_type 'application/json'
+end
+
+
+# Homepage
 get '/' do
-  "Hello!"
+  "/employees => List of employees"
+end
+
+# List all employees at http://localhost:4567/employees
+get '/employees' do
+  Employee.all.to_json
+end
+
+# List an individual employee at http://localhost:4567/employees/:id
+get '/employees/:id' do
+  employee = Employee.find(params["id"])
+
+  employee.to_json
 end
--- development.rb
+++ development.rb
@@ -1,17 +1,22 @@
 require './migrations.rb'
 require './department.rb'
+require 'pry'
 
 ActiveRecord::Base.establish_connection(
   adapter: 'sqlite3',
-  database: 'development.sqlite3'
+  database: 'db.sqlite3'
 )
 
-d = Employee.create!(name: "Doug")
+# Initial DB seed, excluded after creation
 
-e = Employee.create!(name: "Elanor")
-
-t = Department.create!(name: "Telecom")
-
-t.employees = [d, e]
-
-t.save!
+# emp_1 = Employee.create!(name: "Bob", email: "bob@mail.com", phone: "111-111-1111", salary: 50000.00)
+# emp_2 = Employee.create!(name: "Linda", email: "linda@mail.com", phone: "222-222-2222", salary: 40000.00)
+# emp_3 = Employee.create!(name: "Tina", email: "tina@mail.com", phone: "333-333-3333", salary: 30000.00)
+# emp_4 = Employee.create!(name: "Gene", email: "gene@mail.com", phone: "444-444-4444", salary: 20000.00)
+# emp_5 = Employee.create!(name: "Louise", email: "louise@mail.com", phone: "555-555-5555", salary: 10000.00)
+#
+# t = Department.create!(name: "Bob's Burgers")
+#
+# t.employees = [emp_1, emp_2, emp_3, emp_4, emp_5]
+#
+# t.save!
