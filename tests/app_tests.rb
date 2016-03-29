c449b67dd87419155add5057a0766eb99d570503
Nadia Barbosa <nadiabarbosa@me.com>
2016-03-29 16:19:47 -0400

    Tests for list/select endpoints

--- tests/app_tests.rb
+++ tests/app_tests.rb
@@ -1,10 +1,10 @@
 require "./tests/test_helper"
 require './migrations'
 require './app'
-
 require './company'
 require './department'
 require './employee'
+require 'pry'
 
 # Initialize test DB
 ActiveRecord::Base.establish_connection(
@@ -25,15 +25,36 @@ class AppTest < Minitest::Unit::TestCase
     Sinatra::Application
   end
 
-  def test_it_says_hello_world
-    get '/'
-    assert last_response.ok?
-    assert_equal 'I am Groot', last_response.body
+  def setup
+    begin CompanyDataMigration.migrate(:down); rescue; end
+    CompanyDataMigration.migrate(:up)
+
+    @emp_1 = Employee.create!(name: "Bob", email: "bob@mail.com", phone: "111-111-1111", salary: 50000.00)
+    @emp_2 = Employee.create!(name: "Linda", email: "linda@mail.com", phone: "222-222-2222", salary: 40000.00)
+    @emp_3 = Employee.create!(name: "Tina", email: "tina@mail.com", phone: "333-333-3333", salary: 30000.00)
+    @emp_4 = Employee.create!(name: "Gene", email: "gene@mail.com", phone: "444-444-4444", salary: 20000.00)
+    @emp_5 = Employee.create!(name: "Louise", email: "louise@mail.com", phone: "555-555-5555", salary: 10000.00)
+
+    @t = Department.create!(name: "Bob's Burgers")
+    @t.employees = [@emp_1, @emp_2, @emp_3, @emp_4, @emp_5]
+    @t.save!
   end
 
-  def test_it_says_hello_to_a_person
-    get '/', :name => 'Simon'
-    assert last_response.body.include?('Groot')
-    assert_equal 'I am Groot', last_response.body
+
+  def test_can_list_employees_through_endpoint
+    # Replicate web app at current endpoint
+    response = get '/employees'
+
+    assert response.ok?
+    assert_equal @t.employees.to_json, response.body
+    # binding.pry
   end
+
+  def test_can_list_an_individual_employee_through_endpoint
+    response = get '/employees/1'
+
+    assert response.ok?
+    assert_equal @t.employees.find_by(1).to_json, response.body
+  end
+
 end
